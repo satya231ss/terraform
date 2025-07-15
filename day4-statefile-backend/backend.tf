7fbc0ca6a15bbabda5fc1bc7928b18cd3b9e7172
Satya <satya231ss@gmail.com>
2025-07-15 07:21:23 +0530

    new changes

--- day4-statefile-backend/backend.tf
+++ day4-statefile-backend/backend.tf
@@ -3,10 +3,16 @@ terraform {
     bucket = "rajasaty"
     key = "terraform.tfstate"
     region = "us-east-1"
+<<<<<<< HEAD
     #use_lockfile = true
     dynamodb_table = "satya"
      encrypt = true
     
+=======
+    use_lockfile = true
+    #dynamodb_table = "satya"
+    encrypt = true
+>>>>>>> 48d4d03 (new changes)
   }
 }
 
--- day4-statefile-backend/main.tf
+++ day4-statefile-backend/main.tf
@@ -9,6 +9,15 @@ resource "aws_instance" "test" {
   ami = "ami-05ffe3c48a9991133"
   instance_type = "t3.micro"
   tags = {
+<<<<<<< HEAD
     Name = "Day4"
   }
 }
+=======
+  Name = "Day4"
+  }
+} 
+resource "aws_vpc" "vc" {
+  cidr_block = "10.0.1.0/24"
+}
+>>>>>>> 48d4d03 (new changes)
