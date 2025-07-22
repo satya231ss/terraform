dab9e0c7fd283ae945ceaac9a66ba1b50548a615
Satya <satya231ss@gmail.com>
2025-07-22 07:45:06 +0530

    foldr name changed

---
+++ Day-8-Data_source/main.tf
@@ -0,0 +1,16 @@
+data "aws_subnet" "sub" {
+  filter {
+    name   = "tag:Name"
+    values = ["dev"]
+  }
+
+}
+
+resource "aws_instance" "name" {
+  ami           = "ami-05ffe3c48a9991133"
+  instance_type = "t3.micro"
+  subnet_id     = data.aws_subnet.sub.id
+  tags = {
+    name = "frontend"
+  }
+}
